b21df107ed26090f95d1e8a0809abaea03ead228
Matheus Gois <mattheusgoiis@gmail.com>
2025-04-04 00:24:00 -0300

    feat: implement persist current tournament

--- src/stores/tournaments/index.tsx
+++ src/stores/tournaments/index.tsx
@@ -1,9 +1,18 @@
 import { create } from "zustand"
+import { persist } from "zustand/middleware"
 import { TournamentState } from "./types"
 
-export const useTournamentStore = create<TournamentState>()((set) => ({
-  tournaments: [],
-  currentTournament: null,
-  setCurrentTournament: (tournament) => set({ currentTournament: tournament }),
-  setTournaments: (tournaments) => set({ tournaments })
-}))
+export const useTournamentStore = create<TournamentState>()(
+  persist(
+    (set) => ({
+      tournaments: [],
+      currentTournament: null,
+      setCurrentTournament: (tournament) => set({ currentTournament: tournament }),
+      setTournaments: (tournaments) => set({ tournaments })
+    }),
+    {
+      name: "tournament-storage",
+      partialize: (state) => ({ currentTournament: state.currentTournament })
+    }
+  )
+)
