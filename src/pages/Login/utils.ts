4e22cda8fcb263d350533db2ee6899dd3b7026cb
Matheus Gois <mattheusgoiis@gmail.com>
2025-04-03 23:48:32 -0300

    fix: remove duplicated archive

--- src/pages/Login/utils.ts
+++ src/pages/Login/utils.ts
@@ -1,8 +1,8 @@
 import { z } from 'zod';
 
 export const loginSchema = z.object({
-  name: z.string().min(1, 'Nome é obrigatório'),
-  password: z.string().min(6, 'A senha deve ter no mínimo 6 caracteres'),
+  name: z.string().nonempty({ message: 'Nome é obrigatório' }),
+  password: z.string().nonempty({ message: 'Senha é obrigatória' }),
 });
 
-export type LoginFormValues = z.infer<typeof loginSchema>; 
+export type LoginFormValues = z.infer<typeof loginSchema>;
--- src/pages/Login/utils.tsx
+++
@@ -1,8 +0,0 @@
-import { z } from 'zod';
-
-export const loginSchema = z.object({
-  name: z.string().nonempty({ message: 'Nome é obrigatório' }),
-  password: z.string().nonempty({ message: 'Senha é obrigatória' }),
-});
-
-export type LoginFormValues = z.infer<typeof loginSchema>;
